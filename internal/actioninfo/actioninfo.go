3450386afb6d4e435d374341586ff782a1461cf4
Victor Ginzburg <v-viktor456@mail.ru>
2025-03-03 14:47:14 +0300

    implement DataParser interface and Info function in actioninfo

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,11 +1,22 @@
 package actioninfo
 
-import (
-	...
-)
+import "fmt"
 
 // создайте интерфейс DataParser
-...
+type DataParser interface {
+	Parse(string) error
+	ActionInfo() string
+}
 
 // создайте функцию Info()
-...
+func Info(dataset []string, dp DataParser) {
+	for _, datastring := range dataset {
+		err := dp.Parse(datastring)
+
+		if err != nil {
+			continue
+		}
+
+		fmt.Println(dp.ActionInfo())
+	}
+}
